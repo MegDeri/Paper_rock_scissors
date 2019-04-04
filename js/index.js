704e3d32af3092592dee18a4e3bf37f1142d6e62
Magda Aderibigbe <magdaaderibigbe@gmail.com>
2019-04-04 22:12:39 +0300

    Changes js

--- js/index.js
+++ js/index.js
@@ -25,40 +25,40 @@ var params = {
 function getComputerChoice() {
   var choices = ['rock', 'paper','scissors'];
   var randomNumber = Math.floor(Math.random() * 3);
-    return choices[randomNumber];                         
+  return choices[randomNumber];                         
 }
 
 //User choice
 function playerMove(userChoice) {
   if (params.pastRound < params.roundsNumber ) {
     params.pastRound++;
-     params.player = userChoice;
-     params.computerChoice = getComputerChoice();
-      whoWins(params.player, params.computerChoice);
-      showScore();
-      showWinner();
-      addRecord();
-      createTable()
-    }
+    params.player = userChoice;
+    params.computerChoice = getComputerChoice();
+    whoWins(params.player, params.computerChoice);
+    showScore();
+    showWinner();
+    addRecord();
+    createTable()
+  }
 }
  
  //Three sets of nested if-else statements for when user picks rock, paper, or scissors
 function whoWins() {
   if (params.player === params.computerChoice ) {
-  resultDiv.innerHTML = "You choose " + params.player + " Computer: " + params.computerChoice + " . It's a tie!"; 
+    resultDiv.innerHTML = "You choose " + params.player + " Computer: " + params.computerChoice + " . It's a tie!"; 
   } else if (((params.player === 'rock') && (params.computerChoice === 'scissors')) || ((params.player === 'paper') && (params.computerChoice === 'rock')) || ((params.player === 'scissors') && (params.computerChoice === 'paper'))) {
-  resultDiv.innerHTML = "You choose " + params.player + " Computer: " + params.computerChoice + " . You win!";
-  params.userScore++;
+    resultDiv.innerHTML = "You choose " + params.player + " Computer: " + params.computerChoice + " . You win!";
+    params.userScore++;
   } else {
-  resultDiv.innerHTML = "You choose " + params.player + " Computer: " + params.computerChoice + " . Computer wins!";
-  params.computerScore++;
+    resultDiv.innerHTML = "You choose " + params.player + " Computer: " + params.computerChoice + " . Computer wins!";
+    params.computerScore++;
   } 
 }
   
 function showScore() {
-    userScoreSpan.innerHTML = params.userScore;
-    computerScoreSpan.innerHTML = params.computerScore;
-    newGame.innerHTML = params.pastRound + '/' + params.roundsNumber;
+  userScoreSpan.innerHTML = params.userScore;
+  computerScoreSpan.innerHTML = params.computerScore;
+  newGame.innerHTML = params.pastRound + '/' + params.roundsNumber;
 }
 
 function showWinner() {
@@ -92,42 +92,40 @@ for (var i = 0; i < closeButtons.length; i++) {
 document.querySelector('#modal-overlay').addEventListener('click', hideModal);
 
 //Propagation modals
-for (var i = 0; i < modals.length; i++) {
-  modals[i].addEventListener('click', function(event) {
+  modals.addEventListener('click', function(event) {
     event.stopPropagation();
   });
-};
+
 
 //Reset game when player clicks 'New game' button
 function resetGame() {
-   params.pastRound = 0;
-   params.userScore = 0;
-   params.computerScore = 0;
-   userScoreSpan.innerHTML = params.userScore;
-   computerScoreSpan.innerHTML = params.computerScore;
-   output.innerHTML = 'Click one of the buttons and start!';
-   resultDiv.innerHTML = 'Result:';
-   newGame.innerHTML = params.pastRound + '/';
-   winnerLast.innerHTML = "New game";
-   params.progress = [];
+  params.pastRound = 0;
+  params.userScore = 0;
+  params.computerScore = 0;
+  userScoreSpan.innerHTML = params.userScore;
+  computerScoreSpan.innerHTML = params.computerScore;
+  output.innerHTML = 'Click one of the buttons and start!';
+  resultDiv.innerHTML = 'Result:';
+  newGame.innerHTML = params.pastRound + '/';
+  winnerLast.innerHTML = "New game";
+  params.progress = [];
 };
 
 //Connection to button "New game" and display window.prompt with nr of rounds.
-restartGame.addEventListener('click', function(){
+restartGame.addEventListener('click', function() {
   params.roundsNumber = window.prompt('Set number of rounds.');
-      if (params.roundsNumber != null) {
+    if (params.roundsNumber != null) {
       newGame.innerHTML = params.pastRound + '/' + params.roundsNumber;
-      }
-      resetGame();
+    }
+  resetGame();
  });
 
 //Connection to buttons: 'Paper', 'Rock', 'Scissors'
-for( var i = 0; i < buttons.length; i++ ){
+for (var i = 0; i < buttons.length; i++ ) {
   buttons[i].addEventListener('click', function(event) {
-    
     var buttonMove = event.target.getAttribute('data-move');
     playerMove(buttonMove);
-});
+  });
 }
 
 function createTable() {
@@ -139,8 +137,8 @@ function createTable() {
     params.progress[i].computerChoice + '</td><td>' +
     params.progress[i].playerScore + ' : ' + params.progress[i].compScore + '</td></tr>'
   }
-    newHTML += '</tbody></table>';
-    tableModal.innerHTML = newHTML;
+  newHTML += '</tbody></table>';
+  tableModal.innerHTML = newHTML;
 }
 
 function addRecord() {
